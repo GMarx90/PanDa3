5957065267f3fd49d276816a69706a90b53d95c5
Marx <grzegorz.marx@capgemini.com>
2023-04-02 10:28:17 +0200

    Initial commit

---
+++ src/main/java/com/example/panda3/PanDa3Application.java
@@ -0,0 +1,13 @@
+package com.example.panda3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanDa3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PanDa3Application.class, args);
+    }
+
+}
